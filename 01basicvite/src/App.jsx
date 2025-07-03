eb6ed364445f2972c599df16dc309d7f95a33254
rahul-brahmane <rahul.brahmane2004@gmail.com>
2025-07-03 15:42:26 +0530

    js comp with react

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,14 +1,14 @@
 
 import Youtube from "./Youtube";
 
-
 function App() {
+  const username = "rahul"
 
 
   return (
     <>
-      <h1>Vite React App</h1>
-      <h1>Vite React App</h1>
+      <h1>Vite React App {2+2}</h1>
+      <h1>Vite React App {username}</h1>
       <Youtube/>
     </>
   );
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -3,6 +3,33 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "blank",
+  },
+  children: "Click me to visit google",
+};
+
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom React App</h1>
+        </div>
+    )
+}
+
+const AnotherElement = (
+<a href="https://google.com" target = '_blank'>Visit Google</a>
+)
+
+const areactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click to visit google'
+)
+
 createRoot(document.getElementById('root')).render(
  
     <App />
