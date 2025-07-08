d7ad2b9da5fcb057a4b4aec57468d54f179ef05d
rahul-brahmane <rahul.brahmane2004@gmail.com>
2025-07-08 12:43:42 +0530

    completed with UI

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
   }, [length, numberAllowed, charAllowed]);
 
   return (
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-lime-300">
       <h1 className="text-white text-center my-3">Password Generator</h1>
 
       <div className="flex shadow rounded-lg overflow-hidden mb-4">
